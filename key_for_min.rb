f4d59ecdd107a331bacb3aaeceecdcef3f71cbea
JillKlatt <klatt.jill@gmail.com>
2021-02-06 21:10:53 -0600

    commit tree b36d91140b9712c8217a7f8fc63e48e8a47c0e26

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,38 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+require "pry"
 
-end
+# def key_for_min_value(name_hash)
+#     smallest_key = nil 
+#     smallest_val = Float::INFINITY
+#     smallest_val = nil
+#     name_hash.each do |k, v|
+#         if v < smallest_val
+#             smallest_val = v
+#             lowest_key = k
+#         end
+#     end
+#     smallest_key
+#     #binding.pry
+# end
+
+def key_for_min_value(name_hash)
+    if name_hash.length == 0
+      return nil
+    else
+      array = []
+      name_hash.each do |key, value|
+        array << value
+      end
+      min_num = array[0]
+      array.each do |value|
+        min_num = value if value < min_num
+      end
+      name_hash.each do |key, value|
+        if min_num == value
+          return key
+        end
+      end
+    end
+  end
